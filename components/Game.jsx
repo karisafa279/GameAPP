e5be4f22571d749b82c65b1934fb17e961ac795d
karisafa279 <32474931+karisafa279@users.noreply.github.com>
2022-04-25 19:54:40 -0600

    Avacne 2

--- components/Game.jsx
+++ components/Game.jsx
@@ -1,17 +1,36 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Number from "./Number";
 
-export default Game = ({ randomNumbers }) => {
+export default Game = ({ randomNumbersCount }) => {
+
+    const [randomNumbers, setRandomNumbers] = useState([]);
+    const [target, setTarget] = useState(0);
+    const [selectedNumbers, setSelectedNumbers] = useState([]);
+    
+    useEffect(() => console.log(selectedNumbers), [selectedNumbers]);
     //const target = 10 + Math.floor(40 * Math.random()); //10...50
-    const numbers = Array.from({ length: randomNumbers }).map(() => 1 + Math.floor(10 * Math.random()));
-    const target = numbers.slice(0, randomNumbers -2).reduce( (acc, cur) => acc + cur, 0);
+    //const numbers = Array.from({ length: randomNumbers }).map(() => 1 + Math.floor(10 * Math.random()));
+    //const target = numbers.slice(0, randomNumbers -2).reduce( (acc, cur) => acc + cur, 0);
+
+    useEffect ( () => {
+        const numbers = Array.from({ length: randomNumbersCount }).map(() => 1 + Math.floor(10 * Math.random()));
+        const target = numbers.slice(0, randomNumbersCount -2).reduce( (acc, cur) => acc + cur, 0);
+
+        setRandomNumbers(numbers);
+        setTarget(target);
+    }, []);
+    
+    const isNumberSelected = numberIndex => selectedNumbers.some(number => number === numberIndex);
+    const selectNumber = number => (
+        setSelectedNumbers([...selectedNumbers, number]));
 
     return (
         <View>
             <Text style={styles.target}>{target}</Text>
             <View style={styles.randomContainer}>
-                {numbers.map((number, index) => (
-                    <Text key={index} style={styles.random}>{number}</Text>
+                {randomNumbers.map((number, index) => (
+                    <Number key={index} id={index} number={number} isSelected={isNumberSelected(index)} onSelected={selectNumber} />
                 ))}
             </View>
         </View>
